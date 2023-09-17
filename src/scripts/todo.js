cb2e0379a01978c12dee44d545cc1537ac5116b9
bigacz <sbigacz@gmail.com>
2023-09-18 01:33:14 +0200

    initial

---
+++ src/scripts/todo.js
@@ -0,0 +1,13 @@
+class Note {
+    constructor(title, description, dueDate, creationDate, notes, state, priority, project) {
+        this.title = title; 
+        this.description = description; 
+        this.dueDate = dueDate;
+        this.creationDate = creationDate; 
+        this.notes = notes; 
+        this.state = state; 
+        this.priority = priority;
+        this.project = project;
+    }
+}
+
