85a55bfacbfb2c8efeffbf91366007e9568b4e89
bigacz <sbigacz@gmail.com>
2023-09-18 23:58:02 +0200

    Add project class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import Project from './scripts/todo';
+
+Project.addProject('zero');
+Project.addProject('raz');
+Project.addProject('dwa');
+Project.addProject('trzy');
+
+const projects = Project.getAllProjects();
+
+console.log(Project.getAllProjects());
--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -1,5 +1,5 @@
-class Note {
-    constructor(title, description, dueDate, creationDate, notes, state, priority, project) {
+class Todo {
+    constructor(title, description, dueDate, creationDate, notes, state, priority, todoId, projectId) {
         this.title = title; 
         this.description = description; 
         this.dueDate = dueDate;
@@ -7,7 +7,45 @@ class Note {
         this.notes = notes; 
         this.state = state; 
         this.priority = priority;
-        this.project = project;
+        this.todoId = todoId;
+        this.projectId = projectId;
     }
 }
 
+class Project {
+    constructor(title) {
+        this.title = title;
+        this.id = Project.projectId;
+        Project.projectId++;
+    }
+
+    static all = [];
+    static projectId = 0;
+    
+    static addProject(title) {
+        const newProject = new Project(title);
+        console.log(newProject);
+
+        this.all.push(newProject);
+    }
+    static getAllProjects() {
+        return this.all;
+    }
+    static getProject(id) {
+        const projectIndex = this.all.findIndex((e) => e.id === id);
+
+        return this.all[projectIndex];
+    }
+
+    todoId = 0;
+    todos = [];
+
+    addTodo(title, description, dueDate, creationDate, notes, priority) {
+        const newNote = new Todo(title, description, dueDate, creationDate, notes, 0, priority, this.todoId, this.id);
+        this.todoId++;
+
+        this.todos.push(newNote);
+    }
+}
+
+export default Project
